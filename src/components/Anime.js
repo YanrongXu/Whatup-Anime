3e5de8f06dae5d7934e887bfd46afc568712408f
Danny Xu <xuyanrong@me.com>
2021-05-18 06:26:25 -0400

    anime app init

---
+++ src/components/Anime.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DEFAULT_IMAGE = 'https://miro.medium.com/max/1200/1*CDb7OD7D50e27r5F56xGcA.jpeg'
+
+const Anime = ({anime}) => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
