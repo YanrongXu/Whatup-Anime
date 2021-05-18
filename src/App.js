65552df61aeb3694130359d0f02bdd09c53a87d2
Danny Xu <xuyanrong@me.com>
2021-05-18 07:10:37 -0400

    getting data from Anime API

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Anime from './components/Anime';
+import react, { useState, useEffect } from 'react';
+
+const ANIME_API = 'https://api.jikan.moe/v3'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [animes, setAnimes] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    fetch(`${ANIME_API}/search/anime?q=fruit`)
+      .then(response => response.json())
+      .then(animeData => {
+        setAnimes(animeData.results)
+        setLoading(false)
+      })
+  }, [])
+
   return (
     <div className="App">
       <Header text='Whatup Anime' />
-      <Anime />
+      <div className="animes">
+        {loading && !errorMessage ? (<span>Loading...</span>) :
+        errorMessage ? (<div className="errorMessage">{errorMessage}</div>) :
+        (animes.map((anime, index) => (
+          <Anime key={`${index}-${anime.title}`} anime={anime} />
+        )))}
+      </div>
     </div>
   );
 }
--- src/components/Anime.js
+++ src/components/Anime.js
@@ -3,13 +3,13 @@ import React from 'react'
 const DEFAULT_IMAGE = 'https://miro.medium.com/max/1200/1*CDb7OD7D50e27r5F56xGcA.jpeg'
 
 const Anime = ({anime}) => {
-
+    const poster = anime.image_url === "N/A" ? DEFAULT_IMAGE : anime.image_url
     return (
         <div className='anime'>
-            <h2>Fruits Basket</h2>
+            <h2>{anime.title}</h2>
             <div>
                 <img width='200' height="267" alt={`anime name`}
-                    src={DEFAULT_IMAGE} />
+                    src={poster} />
             </div>
         </div>
     )
