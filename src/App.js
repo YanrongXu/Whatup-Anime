01ae92bd9f4f277a864915e714cce28b72512853
Danny Xu <xuyanrong@me.com>
2021-05-18 06:31:30 -0400

    create an temp temple for anime

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Anime from './components/Anime';
 
 function App() {
   return (
     <div className="App">
       <Header text='Whatup Anime' />
+      <Anime />
     </div>
   );
 }
--- src/components/Anime.js
+++ src/components/Anime.js
@@ -5,8 +5,14 @@ const DEFAULT_IMAGE = 'https://miro.medium.com/max/1200/1*CDb7OD7D50e27r5F56xGcA
 const Anime = ({anime}) => {
 
     return (
-        <div>
-            
+        <div className='anime'>
+            <h2>Fruits Basket</h2>
+            <div>
+                <img width='200' height="267" alt={`anime name`}
+                    src={DEFAULT_IMAGE} />
+            </div>
         </div>
     )
 }
+
+export default Anime
