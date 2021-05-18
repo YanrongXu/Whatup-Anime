d77cad567938c698c5a10faeed30820a839ed20d
Danny Xu <xuyanrong@me.com>
2021-05-18 07:51:56 -0400

    create thje seach function for the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Anime from './components/Anime';
+import Header from './Header';
+import Anime from './Anime';
+import Search from './Search'
 import react, { useState, useEffect } from 'react';
 
 const ANIME_API = 'https://api.jikan.moe/v3'
@@ -20,11 +20,24 @@ function App() {
       })
   }, [])
 
+  const search = searchValue => {
+    setLoading(true)
+    setErrorMessage(null)
+    fetch(`${ANIME_API}/search/anime?q=${searchValue}`)
+      .then(res => res.json())
+      .then(animeData => {
+        setAnimes(animeData.results)
+        setLoading(false)
+      })
+  }
+
   return (
     <div className="App">
       <Header text='Whatup Anime' />
+      <Search search={search} />
+      <p className='App-intro'>What is u favorite Anime, Try Sword Art Online</p>
       <div className="animes">
-        {loading && !errorMessage ? (<span>Loading...</span>) :
+        {loading && !errorMessage ? (<div>Loading...</div>) :
         errorMessage ? (<div className="errorMessage">{errorMessage}</div>) :
         (animes.map((anime, index) => (
           <Anime key={`${index}-${anime.title}`} anime={anime} />
